859f2bcbf901bdc23983f2970b2ceae7484c8b41
aristide apetsi <aristideapetsi@gmail.com>
2021-11-07 17:57:57 +0000

    authenticate failed

--- users/list.php
+++ users/list.php
@@ -1,6 +1,6 @@
-<?php require APPROOT . '/views/inc/header.php'; ?>
-<?php flash('post_message'); ?>
-<a href="<?php echo URLROOT; ?>/posts" class="btn btn-light"><i class="fa fa-backward"></i> Back</a>
+<?php require 'inc/header.php'; ?>
+ <?php //flash('post_message'); ?> 
+<a href="<?php echo ''?>/posts" class="btn btn-light"><i class="fa fa-backward"></i> Retour</a>
 <br><br>
 <?php if(isset($data['users']['Error'])){?>
         <h2>No user found </h2>
@@ -12,12 +12,10 @@
   <thead>
     <tr>
       <th scope="col">Nom</th>
+      <th scope="col">Prenoms</th>
       <th scope="col">Email</th>
-      <th scope="col">Username</th>
-      <th scope="col">Token</th>
       <th scope="col">Role</th>
       <th scope="col">Delete</th>
-      <th scope="col">Edit</th>
     </tr>
   </thead>
   <tbody>
@@ -28,11 +26,10 @@
       <td><?php echo $cate->username; ?></td>
       <td><?php echo $cate->token; ?></td>
       <td><?php echo $cate->role; ?></td>
-      <td><a class="btn btn-outline-danger delete_row" href="<?php echo URLROOT; ?>/users/delete/<?php echo $cate->id; ?>">remove</a></td>
-      <td><a class="btn btn-outline-warning delete_row" href="<?php echo URLROOT; ?>/users/edit/<?php echo $cate->id; ?>">edit</a></td>
+      <td><a class="btn btn-outline-danger delete_row" href="/users/delete/<?php echo $cate->id; ?>">remove</a></td>
     </tr>
     <?php endforeach; ?>
     <?php }?>
   </tbody>
 </table>
-<?php require APPROOT . '/views/inc/footer.php'; ?>
+<?php require '/inc/footer.php'; ?>
